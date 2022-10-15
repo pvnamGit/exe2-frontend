8ce7511a864f75099d345c36798e5fed85c95ddf
pvnamGit <pvnam.forwork@gmail.com>
2022-10-16 01:56:38 +0700

    hack con

--- src/config/route.js
+++ src/config/route.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-export const BASE = 'http://motorbike-hiring-backend.herokuapp.com/api/';
+export const BASE = 'https://motorbike-hiring-backend.herokuapp.com/api/';
 
 // auth
 export const REGISTER = `${BASE}auth/signup`;
